fed955ab6dce340361f21f0e89535432d20e05cb
Cesar Loachamin <cesar.loachamin@cobiscorp.com>
2018-12-25 21:17:56 -0500

    Add support to testing using jasmine

---
+++ src/requester-spec.js
@@ -0,0 +1,7 @@
+describe('Requester test', () => {
+
+    it('should check that jasmine is working correctly', () => {
+        const value = true;
+        expect(value).toBeTruthy();
+    });
+});
